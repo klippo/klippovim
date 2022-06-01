740a1011b88a288196b23319e32acf6aa2681a27
klippo <klippo@deny.se>
2022-06-01 22:34:12 +0200

    Updated nvim config to work with latest plugin

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -1,41 +1,6 @@
 -- globals must be set prior to requiring nvim-tree to function
 local g = vim.g
 
-g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-g.nvim_tree_git_hl = 0
-g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
-g.nvim_tree_respect_buf_cwd = 1
-
-
-g.nvim_tree_show_icons = {
-  folders = 1,
-  files = 1,
-  git = 1,
-}
-
-g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    deleted = "",
-    ignored = "◌",
-    renamed = "➜",
-    staged = "✓",
-    unmerged = "",
-    unstaged = "✗",
-    untracked = "★",
-  },
-  folder = {
-    default = "",
-    empty = "",
-    empty_open = "",
-    open = "",
-    symlink = "",
-    symlink_open = "",
-  },
-}
-
 
 
 local present, nvimtree = pcall(require, "nvim-tree")
@@ -55,6 +20,34 @@ local default = {
     enable = true,
     update_cwd = true
   },
+  renderer = {
+    root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" },
+
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+
+        git = {
+          deleted = "",
+          ignored = "◌",
+          renamed = "➜",
+          staged = "✓",
+          unmerged = "",
+          unstaged = "✗",
+          untracked = "★",
+        },
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+      }
+    }
+  }
 }
 
 
